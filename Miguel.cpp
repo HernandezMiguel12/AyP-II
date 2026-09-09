ed2da4424a6dff8b7f5367f8bec3f5b57f52bdd7
HernandezMiguel12 <miguelhp112@gmail.com>
2026-09-09 15:42:07 -0500

    Comprobando rama Miguel

--- Miguel.cpp
+++ Miguel.cpp
@@ -11,6 +11,7 @@ class miguel {
 };
 
 int main () {
-
+    cout<<"Hello, my name is Miguel and this is my first repository on GitHub!"<<endl;
+    cout<<"I am excited to beggin my github journey. This repository will be used for my projects of the 'Algoritms and programming II' course."<<endl;
     return 0;
 }
